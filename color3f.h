febbb09de87d56da5d59e2f59de6bf1cfe8b5cfa
Kristian Dimitrov <kristiandimitrov@Kristians-MacBook-Pro.local>
2019-10-22 00:07:31 +0300

    Commented color3f functions

--- color3f.h
+++ color3f.h
@@ -8,11 +8,29 @@ typedef struct color3f
 	float b;
 }Color3f;
 
+/**
+ * Creates a new color by given RGB components (0.0 - 1.0)
+ * */
 Color3f color3f_new(float r, float g, float b);
+/**
+ * Multiplies each component of the two colors.
+ * */
 Color3f color3f_mul(Color3f a, Color3f b);
+/**
+ * Multiplies each component of color3f by a given coefficient.
+ * */
 Color3f color3f_mul_color(Color3f a, float c);
+/**
+ * Adds the components of the two colors.
+ * */
 Color3f color3f_add(Color3f a, Color3f b);
+/**
+ * Clamps the color value between 0 and 1.
+ * */
 Color3f color3f_clamp(Color3f a);
+/**
+ * Prints the color in the console.
+ * */
 void color3f_print(Color3f a);
 
 #endif
